d8b1affeb9afde0e8ff32c4edbbc7099efaa36cd
Ez_lan <eizs798@gmail.com>
2020-05-18 17:02:26 +0800

    Fix | change footer text

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -3,7 +3,7 @@
     <div class="container my-auto">
         <div class="copyright text-center my-auto">
             <span>
-                Hakcipta Terpelihara &copy;<script>document.write(new Date().getFullYear());</script> Kotakasih Persatuan Hubbulquran</a>
+                Hakcipta Terpelihara &copy;<script>document.write(new Date().getFullYear());</script> UKM Sistem pengurusan Risiko</a>
             </span>
         </div>
     </div>
