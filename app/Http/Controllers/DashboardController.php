3b502aa18f1b0bafb3b951566090b142e06bd487
Ez_lan <eizs798@gmail.com>
2020-06-10 11:14:34 +0800

    Feat | dashboard simple count

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,12 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+Use App\Risk;
+Use App\Department;
+Use App\User;
 
 class DashboardController extends Controller
 {
     public function adminDashboard()
     {
-        return view('dashboard.admin.index');
+        $risks = Risk::all();
+        $departments = Department::all()->count();
+        $operators = User::operator()->count();
+
+        return view('dashboard.admin.index', compact('risks', 'departments', 'operators'));
     }
 
     public function operatorDashboard()
--- resources/views/dashboard/admin/index.blade.php
+++ resources/views/dashboard/admin/index.blade.php
@@ -6,4 +6,59 @@
     <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
 </div>
 
+<div class="row">
+
+    <!-- No of jabatan -->
+    <div class="col-xl-3 col-md-6 mb-4">
+        <div class="card border-left-primary shadow h-100 py-2">
+            <div class="card-body">
+                <div class="row no-gutters align-items-center">
+                    <div class="col mr-2">
+                        <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">jabatan</div>
+                        <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $departments }}</div>
+                    </div>
+                    <div class="col-auto">
+                        <i class="fas fa-people-arrows fa-2x text-gray-300"></i>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- No of operator -->
+    <div class="col-xl-3 col-md-6 mb-4">
+        <div class="card border-left-warning shadow h-100 py-2">
+            <div class="card-body">
+                <div class="row no-gutters align-items-center">
+                    <div class="col mr-2">
+                        <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Operator</div>
+                        <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $operators }}</div>
+                    </div>
+                    <div class="col-auto">
+                        <i class="fas fa-users fa-2x text-gray-300"></i>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- No of Risk -->
+    <div class="col-xl-3 col-md-6 mb-4">
+        <div class="card border-left-danger shadow h-100 py-2">
+            <div class="card-body">
+                <div class="row no-gutters align-items-center">
+                    <div class="col mr-2">
+                        <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Risiko</div>
+                        <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $risks->count() }}</div>
+                    </div>
+                    <div class="col-auto">
+                        <i class="fas fa-exclamation-triangle fa-2x text-gray-300"></i>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</div>
+
 @endsection
